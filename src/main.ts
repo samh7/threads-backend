2bb662cb7539da3a00029a9be1b34ad679ce7609
samh7 <samh74490@gmail.com>
2024-12-28 00:48:37 +0300

    add swagger and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 // import { ValidationErrorFilter } from './filters/db-validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
-  // app.useGlobalFilters(ValidationErrorFilter())
+  app.enableCors(
+    {
+      origin: "http://localhost:4200",
+      credentials: true
+    }
+  )
+  app.use(cookieParser());
+  // swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle("Threads App")
+    .setDescription("Threads messaging app")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
